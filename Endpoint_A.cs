6d78899968c7de7154f96eab9f6914c9f72e14a0
FourSeasonFlora <fourseasonflora@gmail.com>
2022-03-11 13:48:03 -0500

    Changed Comments

--- Endpoint_A.cs
+++ Endpoint_A.cs
@@ -19,18 +19,12 @@ namespace Coding_Assessment_Endpoints
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            // Endpoint_A is called from our Endpoint_C, and passed in the RequestMessage as our HttpRequest req
-
-            // read the RequestMessage from the Body of the HttpRequest and read to the end to grab all the data
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            // desearialize requestBody from a JSON string back into a Dictionary
             var data = JsonSerializer.Deserialize<Dictionary<string, int[]>>(requestBody);
 
-            // sort the array in the Dictionary, and set the value of the Dictionary to the newly sorted array
             data["numbers"] = data["numbers"].OrderByDescending(num => num).ToArray();
 
-            // searialize the Dictionary back into a JSON string and send back Endpoint_C as a response
             return new OkObjectResult(JsonSerializer.Serialize(data));
         }
     }
--- Endpoint_B.cs
+++ Endpoint_B.cs
@@ -19,27 +19,18 @@ namespace Coding_Assessment_Endpoints
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            // Endpoint_B is called from our Endpoint_C, and passed in the RequestMessage as our HttpRequest req
-
-            // make new EvenResult object from our custom built class
             var evenResults = new EvenResult();
 
-            // read the RequestMessage from the Body of the HttpRequest and read to the end to grab all the data
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            // desearialize requestBody from a JSON string back into a Dictionary
             var dictWithSortedArray = JsonSerializer.Deserialize<Dictionary<string, int[]>>(requestBody);
 
-            // grab sorted array from our Dictionary and assign it to the sortedNumbers property of our class
             evenResults.sortedNumbers = dictWithSortedArray["numbers"];
 
-            // sum the even numbers of our sorted array and assign the value to the evenSum property of our class
             evenResults.evenSum = dictWithSortedArray["numbers"].Where(num => num % 2 == 0).Sum();
 
-            // get the average of the sorted array and assign the value to our evenAvg property of our class
             evenResults.evenAvg = dictWithSortedArray["numbers"].Sum() / dictWithSortedArray["numbers"].Count();
 
-            // searialize our evenResults object into a JSON string, and send back as a response
             return new OkObjectResult(JsonSerializer.Serialize(evenResults));
         }
     }
